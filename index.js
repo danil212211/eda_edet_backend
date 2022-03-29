aed81633e99ba897bd6d09c0da4ee18893437407
danil212211 <danil.zelenskiy.2005@mail.ru>
2022-03-29 12:19:25 +0900

    Начальная архитектура юзера
    Пока без заполнения, чисто темплэйт, скоро всё допишу

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 const express= requre("express");
+const user = require("./src/user")
+const cors = require("cors");
 const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.post("/register",async (req,res) => {
+    let ans = await user.register(req);
+    res.status(200).json(ans);
+});
+app.get("/verification",async (req,res) => {
+    let ans = await user.verify(req);
+    res.status(200).json(ans);
+
+});
+
+app.get("/login", async (req,res) => {
+    let ans = await user.login(req);
+    res.status(200).json(ans);
+
+});
+app.listen(port);
---
+++ src/user.js
@@ -0,0 +1,14 @@
+async function reg(req) {
+
+}
+async function log(req) {
+
+}
+async function verification(req) {
+
+}
+module.exports = {
+    register : reg,
+    login    :  log,
+    verify   : verification
+};
